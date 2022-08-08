cad5c41baf54da624b39c9b2fcf0ec941c5238e1
101Amine <aminebecha8@gmail.com>
2022-08-08 20:11:18 +0100

    ..

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,47 @@
-import React from 'react'
 
-export const Header = () => {
+import {Navbar, Container,Nav,NavDropdown} from 'react-bootstrap'
+import './Header.css'
+import Logo from '../assets/imgs/logo.svg'
+
+function Header() {
   return (
-    <div>This is a header </div>
-  )
+    <header className='primary-header'>
+    <div className='container'>
+        <div className='nav-wrapper'>
+            <div className='left-navbar'>
+                <div className='logo'>
+                    <img src={Logo} alt="logo" />
+                </div>
+                <div className='nav'> 
+                    <ul className='nav-list' role="list">
+                            <li><a>Features</a></li>
+                            <li><a>Company</a></li>
+                            <li><a>Careers</a></li>
+                            <li><a>About</a></li>
+                    </ul>
+                </div>
+
+            </div>
+            <div className='actions d-flex'>
+            <div>
+                <button className='button' data-type="inverted">
+                    Login
+                </button>
+            </div>
+                
+            <div >
+
+                <button className='button' >
+                    Register
+                </button>
+                </div>
+            
+            </div>
+        </div>
+    </div>
+    </header>
+    
+  );
 }
+
+export default Header;
