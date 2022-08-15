179d42890308e0f6560fac4186321e8d7453f176
101Amine <aminebecha8@gmail.com>
2022-08-15 04:55:21 -0700

    ..

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,27 @@
+import React from 'react';
+import Navbar from '../ui/Navbar';
+import logo from '../assets/images/logo.svg'
+import "./Header.css"
 
-import {Navbar, Container,Nav,NavDropdown} from 'react-bootstrap'
-import './Header.css'
-import Logo from '../assets/imgs/logo.svg'
+function Header(props) {
+    return (
+        <div className='navigation'>   
+            <div className='Headerlogo'> <img src={logo} alt='' /></div>
+            <Navbar></Navbar>
 
-function Header() {
-  return (
-    <header className='primary-header'>
-    <div className='container'>
-        <div className='nav-wrapper'>
-            <div className='left-navbar'>
-                <div className='logo'>
-                    <img src={Logo} alt="logo" />
-                </div>
-                <div className='nav'> 
-                    <ul className='nav-list' role="list">
-                            <li><a>Features</a></li>
-                            <li><a>Company</a></li>
-                            <li><a>Careers</a></li>
-                            <li><a>About</a></li>
-                    </ul>
-                </div>
 
-            </div>
-            <div className='actions d-flex'>
-            <div>
-                <button className='button' data-type="inverted">
-                    Login
-                </button>
-            </div>
-                
-            <div >
-
-                <button className='button' >
-                    Register
-                </button>
-                </div>
+            <div className='actions'> 
             
+            <button className='btn' onClick={() => console.log('test')} data-type="inverted">
+                Login
+            </button>
+            
+            <button className='btn' onClick={() => console.log('test')}>
+                Register
+            </button>
             </div>
         </div>
-    </div>
-    </header>
-    
-  );
+    );
 }
 
 export default Header;
---
+++ src/menuItems.js
@@ -0,0 +1,42 @@
+export const menuItems = [
+
+  {
+    title: "Features",
+    submenu: [
+      {
+        title: "Todo list",
+      },
+      {
+        title: "Calendar",
+      },
+      {
+        title: "Reminders",
+      },
+      {
+        title: "Planning",
+      },
+    ],
+  },
+  {
+    title: "Company",
+    submenu: [
+      {
+        title: "History",
+      },
+      {
+        title: "Our Team",
+      },
+      {
+        title: "Blog",
+      },
+    ],
+  },
+
+  {
+    title: "Careers",
+  },
+
+  {
+    title: "About",
+  },
+];
